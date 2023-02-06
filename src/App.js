09853bce2b0e22f45f96300e248ef1d9a7ffaa3d
leonace924 <meobius123@gmail.com>
2023-02-06 14:06:42 -0500

    feat: setup  shopify polaris library

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import { Loading } from 'components/Common'
 import { AppRoutes } from 'routes'
+import { AppProvider } from '@shopify/polaris'
+import enTranslations from '@shopify/polaris/locales/en.json'
+import '@shopify/polaris/build/esm/styles.css'
 import 'styles/index.css'
 
 function App() {
   return (
     <React.Suspense fallback={<Loading />}>
-      <AppRoutes />
+      <AppProvider i18n={enTranslations}>
+        <AppRoutes />
+      </AppProvider>
     </React.Suspense>
   )
 }
