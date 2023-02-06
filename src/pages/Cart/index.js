4f14a47f2ea258e2857bcefdf11126194fa2d5bd
leonace924 <meobius123@gmail.com>
2023-02-06 13:59:25 -0500

    feat: add basic cart page

---
+++ src/pages/Cart/index.js
@@ -0,0 +1,9 @@
+const CartPage = () => {
+  return (
+    <div className=" text-rose-800">
+      <div>Cart Page</div>
+    </div>
+  )
+}
+
+export default CartPage
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,14 @@
 import React, { lazy } from 'react'
 import { Route, Routes } from 'react-router-dom'
+
 const PLPPage = lazy(() => import('pages/PLP'))
+const CartPage = lazy(() => import('pages/Cart'))
 
 export const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" exact element={<PLPPage />} />
+      <Route path="/cart" exact element={<CartPage />} />
       {/* <Route path="/login" /> */}
     </Routes>
   )
