17e63c9e0f69e18db90e1b8cedaa771e504a8ada
leonace924 <meobius123@gmail.com>
2023-02-06 14:15:01 -0500

    fix: clean up files

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 // import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
-import reportWebVitals from './pages/reportWebVitals'
+import reportWebVitals from './reportWebVitals'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
