a4265186eaacb30a0511c5a46748773d1e5402a0
leonace924 <meobius123@gmail.com>
2023-02-07 13:20:29 -0500

    chore: clean up old components

--- src/components/Common/Loading/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const Loading = () => {
-  return (
-    <div className="fixed top-0 left-0 z-50 flex items-center justify-center w-full h-screen bg-dark-blue bg-opacity-20">
-      <div className="w-10 h-10 border-b-2 rounded-full animate-spin border-primary text-primary" />
-    </div>
-  )
-}
