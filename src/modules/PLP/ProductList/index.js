eda62deade2d413a69fd3be70519db3ff50b4bbe
leonace924 <meobius123@gmail.com>
2023-02-07 14:30:10 -0500

    feat: add category filter

--- src/modules/PLP/ProductList/index.js
+++ src/modules/PLP/ProductList/index.js
@@ -1,20 +1,29 @@
 import { useCallback, useEffect, useMemo, useState } from 'react'
 import {
   Avatar,
+  Card,
+  ChoiceList,
   ResourceItem,
   Inline,
-  Card,
   Filters,
   Spinner,
   ResourceList,
   Text,
 } from '@shopify/polaris'
 import { useAxios } from 'hooks'
+import { disambiguateLabel, isEmpty } from 'helpers'
+import { SORT_OPTIONS } from 'utils/constant'
 
 export const ProductList = () => {
   const [apiUrl, setAPIUrl] = useState('/products')
   const [queryValue, setQueryValue] = useState('')
   const [sortValue, setSortValue] = useState('ASC')
+  const [selectedCategory, setSelectedCategory] = useState(null)
+
+  const { data: categories } = useAxios({
+    method: 'get',
+    url: '/products/categories',
+  })
 
   const { data, isLoading, error } = useAxios({
     method: 'get',
@@ -23,16 +32,70 @@ export const ProductList = () => {
 
   useEffect(() => {
     setAPIUrl('/products')
+    setSortValue('ASC')
   }, [])
 
   useEffect(() => {
-    setAPIUrl(`/products?sort=${sortValue.toLowerCase()}`)
-  }, [sortValue])
+    const url = `/products${
+      selectedCategory ? `/category/${selectedCategory}` : ''
+    }?sort=${sortValue.toLowerCase()}`
+
+    setAPIUrl(url)
+  }, [selectedCategory, sortValue])
+
+  const handleSortValueChange = useCallback((value) => {
+    setSortValue(value)
+  }, [])
+
+  const handleFiltersQueryChange = useCallback((value) => {
+    setQueryValue(value)
+  }, [])
+
+  const handleQueryValueRemove = useCallback(() => setQueryValue(''), [])
+
+  const handleCategoryRemove = useCallback(() => setSelectedCategory(null), [])
+
+  const handleClearAll = useCallback(() => {
+    handleQueryValueRemove()
+    handleCategoryRemove()
+  }, [handleCategoryRemove, handleQueryValueRemove])
+
+  const filters = [
+    {
+      key: 'category',
+      label: 'Category',
+      filter: (
+        <ChoiceList
+          title="Category"
+          titleHidden
+          choices={categories?.map((category) => ({
+            label: category,
+            value: category,
+          }))}
+          selected={selectedCategory || []}
+          onChange={setSelectedCategory}
+        />
+      ),
+      shortcut: true,
+    },
+  ]
+
+  const appliedFilters = []
+  if (!isEmpty(selectedCategory)) {
+    const key = 'category'
+    appliedFilters.push({
+      key,
+      label: disambiguateLabel(key, selectedCategory),
+      onRemove: handleCategoryRemove,
+    })
+  }
 
   const filteredItems = useMemo(() => {
     if (!data || !data?.length) return []
 
-    return data.filter((item) => item?.title?.includes(queryValue))
+    return data.filter((item) =>
+      item?.title.toLowerCase()?.includes(queryValue.toLowerCase()),
+    )
   }, [queryValue, data])
 
   const renderItem = ({
@@ -76,16 +139,6 @@ export const ProductList = () => {
     )
   }
 
-  const handleFiltersQueryChange = useCallback((value) => {
-    setQueryValue(value)
-  }, [])
-
-  const handleQueryValueRemove = useCallback(() => setQueryValue(''), [])
-
-  const handleClearAll = useCallback(() => {
-    handleQueryValueRemove()
-  }, [handleQueryValueRemove])
-
   if (isLoading & !data)
     return (
       <Spinner accessibilityLabel="Loading Products Spinner" size="small" />
@@ -100,26 +153,20 @@ export const ProductList = () => {
         resourceName={{ singular: 'product', plural: 'products' }}
         items={filteredItems}
         renderItem={renderItem}
+        loading={isLoading}
+        sortValue={sortValue}
+        sortOptions={SORT_OPTIONS}
+        onSortChange={handleSortValueChange}
         filterControl={
           <Filters
             queryValue={queryValue}
-            filters={[]}
-            // appliedFilters={appliedFilters}
+            filters={filters}
+            appliedFilters={appliedFilters}
             onQueryChange={handleFiltersQueryChange}
             onQueryClear={handleQueryValueRemove}
             onClearAll={handleClearAll}
           />
         }
-        sortValue={sortValue}
-        sortOptions={[
-          { label: 'Ascending', value: 'ASC' },
-          { label: 'Descending', value: 'DESC' },
-        ]}
-        onSortChange={(selected) => {
-          setSortValue(selected)
-          console.log(`Sort option changed to ${selected}.`)
-        }}
-        loading={isLoading}
       />
     </Card>
   )
